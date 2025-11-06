b233098b1c6b5609e44537d68928853d62589703
Ten <Ten@DESKTOP-MG3L518>
2025-11-06 13:28:35 +0200

    Add project files.

---
+++ Myfuncs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yakir
+{
+    internal class Myfuncs
+    {
+        public static int MyMax(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Yakir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Targil1();
+        }
+
+        static void Targil1()
+        {
+            int x, y, z;
+            Console.WriteLine("Enter number");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number");
+            y = int.Parse(Console.ReadLine());
+            z = Myfuncs.MyMax(x, y);
+            Console.WriteLine($"Biggest is {z}");
+        }
+    }
+}
