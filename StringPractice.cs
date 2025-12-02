ff993934bd9dbb217cecbbdb190d5772935d89c7
yakir <yakir.israelov13@gmail.com>
2025-12-02 09:35:46 +0200

    המשך

--- StringPractice.cs
+++ StringPractice.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Yakir
@@ -14,6 +15,10 @@ namespace Yakir
             //Console.WriteLine(StringPractice.Targil1("Hello World", 'a'));
             //Console.WriteLine(StringPractice.Targil2("Hello World" , "tgggg lllll"));
             //Console.WriteLine(StringPractice.Targil3("Hello World", "Hello World"));
+            //Console.WriteLine(StringPractice.Targil4("Yakir" , "Yakir"));
+            //Console.WriteLine(StringPractice.Targil5("hello"));
+            //Console.WriteLine(StringPractice.Targil6_a('r'));
+            Console.WriteLine(StringPractice.Targil6_b("hello"));
         }
 
         public static int Targil0(string s)
@@ -57,12 +62,52 @@ namespace Yakir
             bool b = false;
             for (int i = 0; i < s.Length; i++)
             {
-                if (s == str)
+                if (s.Length == str.Length && s[i] == str[i])
                 {
                     b = true;
                 }
             }
             return b;
+        }
+
+        public static string Targil4(string s, string str)
+        {
+            string letter;
+            Console.WriteLine("Write down the letter body");
+            letter = Console.ReadLine();
+            string allletter = "Hello " + s + "\n" + letter + "\n" + "Best Regards," + "\n" + str;
+            return allletter;
+        }
+
+        public static string Targil5(string s)
+        {
+            string str = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                str += s[i];
+            }
+            return str;
+        }
+
+        public static char Targil6_a(Char c)
+        {
+            if (c >= 'a' && c <= 'z')
+            {
+                c = (char)(c - 32);
+            }
+            return c;
+        }
+
+        public static string Targil6_b(string c)
+        {
+            char s = ' ';
+            if (s >= 'a' && s <= 'z')
+            {
+
+                s = (char)(s - 32);
+                c += s;
+            }
+            return c;
         }
     }
 }
