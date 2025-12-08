9bff2ea2b805949a1f2a358e12be299276a1c3bb
yakir <yakir.israelov13@gmail.com>
2025-12-08 17:46:37 +0200

    המשךך

--- Program.cs
+++ Program.cs
@@ -10,44 +10,7 @@ namespace Yakir
         static void Main(string[] args)
         {
             StringPractice.UnitTests();
-            //int year, month, day;
-            //year = int.Parse(Console.ReadLine());
-            //month = int.Parse(Console.ReadLine());
-            //day = int.Parse(Console.ReadLine());
-            //if (year >= 1900 && year <= 2100)
-            //{
-            //    if (month <= 12)
-            //    {
-            //        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-            //        {
-            //            if (day >= 1 && day <= 31)
-            //            {
-            //                Console.WriteLine("Date is ok");
-            //            }
-            //        }
-            //        if (month == 4 || month == 6 || month == 9 || month == 11)
-            //        {
-            //            if (day >= 1 && day <= 30)
-            //            {
-            //                Console.WriteLine("Date is ok");
-            //            }
-            //        }
-            //        if (year % 4 == 0)
-            //        {
-            //            if (day >= 1 && day <= 29)
-            //            {
-            //                Console.WriteLine("Date is ok");
-            //            }
-            //        }
-            //        else
-            //        {
-            //            if (day >= 1 && day <= 28)
-            //            {
-            //                Console.WriteLine("Date is ok");
-            //            }
-            //        }
-            //    }
-            //}
+       
         }
     }
 }
