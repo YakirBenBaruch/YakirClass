69cae694427d0932bfe4a5aa8600cd352038c4ea
yakir <yakir.israelov13@gmail.com>
2026-01-04 11:43:16 +0200

    המשך מערך

--- ArraysPractice.cs
+++ ArraysPractice.cs
@@ -14,7 +14,10 @@ namespace Yakir
             //ArraysPractice.T10_6();
             //ArraysPractice.T10_7();
             //ArraysPractice.T10_8();
-            //ArraysPractice.T_12();
+            //ArraysPractice.T10_12();
+            //ArraysPractice.T10_13();
+            //ArraysPractice.T10_17();
+            ArraysPractice.T10_18();
 
         }
 
@@ -90,7 +93,7 @@ namespace Yakir
             }
         }
 
-        public static void T_12()
+        public static void T10_12()
         {
             int num;
             Console.WriteLine("Enter num");
@@ -110,6 +113,104 @@ namespace Yakir
 
                 Console.WriteLine(arr[i]);
             }
+        }
+
+        public static void T10_13()
+        {
+            int[] arr = new int[100];
+            //א. בודק האם ערך המערך במקום הסידורי מתחלק ב5 ללא שארית
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 5 == 0)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            //ב. בודק האם המספר הסידורי מתחלק ב5 ללא שארית
+
+            //ג
+            for (int i = 0; i < arr.Length; i += 5)
+            {
+
+            }
+        }
+
+        public static void T10_17()
+        {
+            int[] arr = new int[100];
+            int position = 0;
+            int counter = 0;
+            //א
+            if (arr[position] == arr[position - 1])
+            {
+                Console.WriteLine("previous one is equal");
+            }
+
+            if (arr[position] == arr[position + 1])
+            {
+                Console.WriteLine("next one is equal");
+            }
+            //בודק האם הערך שווה לערך הקודם 
+
+            //ב
+            for (int i = 0; i < 10; i++)
+            {
+                if (arr[position] == arr[position + i])
+                {
+                    counter = counter + 1;
+                    Console.WriteLine(counter);
+                }
+            }
+            //האם הערך שווה לערך פלוס מספרו הסידורי בלולאה
+
+            //ג
+            for (int i = 0; i < 20; i++)
+            {
+                if (arr[position] < arr[position + i])
+                {
+                    Console.WriteLine(position + i);
+                }
+            }
+
+            //ד
+            for (int i = 1; i <= 20; i++)
+            {
+                if (arr[position] < arr[position + i])
+                {
+                    if (arr[position + i] - arr[position] == i)
+                    {
+                        Console.WriteLine(position + i);
+                    }
+                  
+                }
+            }
+        }
+
+        public static void T10_18()
+        {
+            char[] c = new char[20];
+            int Count = 0;
+            for (int i = 0; i < c.Length; i++)
+            {
+                Console.WriteLine("Enter char");
+                c[i] = char.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < c.Length - 1; i++)
+            {
+                if (c[c.Length - 1] == c[i] && c[i + 1] == c[c.Length - 2])
+                {
+                    Count++;
+                }
+            }
+            Console.WriteLine(Count);
         }
     }
 }
