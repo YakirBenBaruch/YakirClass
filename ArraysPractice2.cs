1e63e777b25723a3848b9b8eba44a7179f2543ea
yakir <yakir.israelov13@gmail.com>
2026-01-08 17:05:26 +0200

    h

--- ArraysPractice2.cs
+++ ArraysPractice2.cs
@@ -12,6 +12,7 @@ namespace Yakir
         public static void UnitTest()
         {
             //ArraysPractice2.T10_19();
+            ArraysPractice2.T10_23();
         }
 
         public static void T10_19()
@@ -27,7 +28,7 @@ namespace Yakir
             Pos = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter num of the Kobia");
             Kobia = int.Parse(Console.ReadLine());
-            if (Kobia >= Index[Pos + Kobia -1])
+            if (Kobia >= Index[Pos + Kobia - 1])
             {
                 Pos += Kobia;
             }
@@ -40,14 +41,42 @@ namespace Yakir
 
         public static void T10_22()
         {
+            /*
             int[] arr = new int[10];
             for (int i = 0; i < arr.Length - 1; i++)
             {
                 arr[i + 1] = arr[i];
             }
             //א. איברי המערך בסוף התוכנית יהיו שווים לערך הראשון במקום הראשון
+            */
 
             //ב
+            int[] arr = new int[10];
+
+            int last = arr[arr.Length - 1];
+
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+
+            arr[0] = last;
+        }
+
+        public static void T10_23()
+        {
+            int People;
+            Console.WriteLine("Enter num of people");
+            People = int.Parse(Console.ReadLine());
+            int[] Num = new int[People];
+            string[] name = new string[People];
+            for (int i = 20; i <= 75; i += 20)
+            {
+                for (int j = 0; j < Num.Length; j++)
+                {
+                    name[j] = Console.ReadLine();
+                }
+            }
 
         }
     }
