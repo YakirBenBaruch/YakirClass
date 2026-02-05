83bc7b446123d22516d3dce67f19d299b6374a5e
YakirBenBaruch <yakir.israelov13@gmail.com>
2026-02-05 18:20:02 +0000

    ggg

--- ClassPractice.cs
+++ ClassPractice.cs
@@ -11,14 +11,7 @@ namespace Yakir
         public static void UnitTest()
         {
             CssGrades student1;
-            student1 = new CssGrades();
-            student1.name = "Josh";
-            student1.magen371 = 82;
-            student1.bagrut371 = 98;
-            student1.magen271 = 87;
-            student1.bagrut271 = 17;
-            student1.proj = 100;
-
+            student1 = new CssGrades("Josh", 82, 98, 87, 17, 100);
             int g = student1.Calc371();
             Console.WriteLine(g);
             Console.WriteLine(student1.Calc271());
