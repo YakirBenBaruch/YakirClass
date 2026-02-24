2657e4dc6b3fba1a2574b1f18176175c74337432
yakir <yakir.israelov13@gmail.com>
2026-02-24 09:41:36 +0200

    bbbbbbbb

---
+++ Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yakir
+{
+    public class Rectangle
+    {
+        private Point bottomLeft;
+        private Point topRight;
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            this.bottomLeft = new Point(bottomLeft);
+            this.topRight = new Point(topRight);
+        }
+    }
+}
